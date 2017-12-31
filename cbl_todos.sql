8ba021ff2eec05cbccfff6c8fc7c6157568d7ab5
Chris Alfano <chris@jarv.us>
2017-12-31 03:36:47 +0000

    Add TODOs for Demo Teacher

--- cbl_todos.sql
+++ cbl_todos.sql
@@ -18,7 +18,7 @@ CREATE TABLE `cbl_todos` (
   `Completed` tinyint(1) NOT NULL DEFAULT '0',
   `Cleared` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 INSERT INTO `cbl_todos` VALUES (1,'Slate\\CBL\\Tasks\\Todo','2017-12-31 03:28:45',2,2,NULL,'Personal todo','2018-01-01 05:00:00',0,0);
@@ -27,6 +27,10 @@ INSERT INTO `cbl_todos` VALUES (3,'Slate\\CBL\\Tasks\\Todo','2017-12-31 03:29:13
 INSERT INTO `cbl_todos` VALUES (4,'Slate\\CBL\\Tasks\\Todo','2017-12-31 03:29:27',2,2,1,'Complete Science Studio todo','2017-12-31 05:00:00',1,0);
 INSERT INTO `cbl_todos` VALUES (5,'Slate\\CBL\\Tasks\\Todo','2017-12-31 03:29:40',2,2,NULL,'Complete personal todo','2018-01-02 05:00:00',1,0);
 INSERT INTO `cbl_todos` VALUES (6,'Slate\\CBL\\Tasks\\Todo','2017-12-31 03:29:51',2,2,2,'Complete ELA studio todo','2017-12-22 05:00:00',1,0);
+INSERT INTO `cbl_todos` VALUES (7,'Slate\\CBL\\Tasks\\Todo','2017-12-31 03:34:57',23,23,NULL,'Teacher personal todo','2018-01-01 05:00:00',0,0);
+INSERT INTO `cbl_todos` VALUES (8,'Slate\\CBL\\Tasks\\Todo','2017-12-31 03:35:10',23,23,1,'Teacher science studio todo','2018-01-01 05:00:00',0,0);
+INSERT INTO `cbl_todos` VALUES (9,'Slate\\CBL\\Tasks\\Todo','2017-12-31 03:35:21',23,23,2,'Teacher ELA studio todo','2018-02-01 05:00:00',0,0);
+INSERT INTO `cbl_todos` VALUES (10,'Slate\\CBL\\Tasks\\Todo','2017-12-31 03:35:35',23,23,NULL,'Complete teacher personal todo','2018-02-01 05:00:00',0,0);
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
